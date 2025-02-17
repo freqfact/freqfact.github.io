5b6d5c523ab750a398ef63cccae9fa1d9565c513
Joshua Friese <phist@phist-m1.local>
2025-02-17 16:06:17 -0500

    fix deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://freqfact.github.io',
+  base: '/',
   vite: {
     plugins: [tailwindcss()]
   }
